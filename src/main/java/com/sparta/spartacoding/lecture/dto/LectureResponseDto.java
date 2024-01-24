de82b5d0ebeaa4cd621567fd570934cfcb4115d4
mingang211 <a989977@naver.com>
2024-01-24 11:00:33 +0900

    Refactor : 수정 총시간, 세부강의 갯수 수정

--- src/main/java/com/sparta/spartacoding/lecture/dto/LectureResponseDto.java
+++ src/main/java/com/sparta/spartacoding/lecture/dto/LectureResponseDto.java
@@ -10,13 +10,15 @@ import java.util.List;
 public class LectureResponseDto {
     private Long lectureId;
     private String lectureTitle;
-    private float lectureTotal;
+    private float lectureAllRunTimeTotal;
+    private int lectureTotal;
     private List<LectureContentsResponseDto> lectureContentsList;
 
     public LectureResponseDto(Lecture lecture){
         this.lectureId = lecture.getLectureId();
         this.lectureTitle = lecture.getLectureTitle();
-        this.lectureTotal = lecture.getLectureContentsList().stream()
+        this.lectureTotal = lecture.getLectureContentsList().size();
+        this.lectureAllRunTimeTotal = lecture.getLectureContentsList().stream()
                 .map(LectureContents::getLectureRunTime)
                 .reduce(0.0f, Float::sum);
         this.lectureContentsList = lecture.getLectureContentsList().stream()
